03b4669f2cdf688afca4decdca628aa65b32c069
ayushi-309 <tayushi751@gmail.com>
2025-01-13 22:50:03 +0530

    operator

--- html.frontpage/javascript/myScript.js
+++ html.frontpage/javascript/myScript.js
@@ -1,13 +1,61 @@
 //Arthemathic operator
-
 var x = 5, y = 2;
 
-console.log("x", x);
-console.log("y", y);
+// console.log("x", x);
+// console.log("y", y);
 
-console.log("addition", x + y);
-console.log("subtraction", x - y);
-console.log("muliplication", x * y);
-console.log("division", x / y);
-console.log("modulus", x % y);
-console.log("exponentiation", x ** y);
+// console.log("addition", x + y);
+// console.log("subtraction", x - y);
+// console.log("muliplication", x * y);
+// console.log("division", x / y);
+// console.log("modulus", x % y);
+// console.log("exponentiation", x ** y);
+
+
+//Relational operator
+
+// var x = 5, y = 2;
+
+// console.log("x", x);
+// console.log("y", y);
+
+// console.log("is x greater than y ?", x > y);
+// console.log("is x less than y ?", x < y);
+// console.log("is x greater than or equal to y ?", x >= y);
+// console.log("is x less than or equal to y ?", x <= y);
+
+//Equality operator
+
+// var x = 5, y = 5;
+// console.log("x is equal to y", x == y);
+
+// Logical operator
+
+//    var x = 2 > 6, y = 9 > 7;
+//     console.log("x", x);
+//     console.log("y", y);
+
+//     console.log("x and y", x && y);
+//     console.log("x or y", x || y);
+
+// Assignment operator
+
+// var x = 5;
+// var y = 2;
+
+// console.log(x,y);
+
+// var x = 3**(3/3);
+// console.log(x);
+
+// var sum = 10 + 5;
+// console.log(sum);
+
+var x = "20";
+var y = "2a";
+
+var sub = x - y;
+// var mul = x * y;
+// var div = x / y;
+
+console.log(isNaN(sub));
