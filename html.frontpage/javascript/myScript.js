e608ed023a0534e1def44ef8a1344dd3c9bc0fd1
ayushi-309 <tayushi751@gmail.com>
2025-01-13 20:06:41 +0530

    arthematic operator

--- html.frontpage/javascript/myScript.js
+++ html.frontpage/javascript/myScript.js
@@ -1,3 +1,13 @@
-var x = 10;
-x = 15;
-console.log(x);
+//Arthemathic operator
+
+var x = 5, y = 2;
+
+console.log("x", x);
+console.log("y", y);
+
+console.log("addition", x + y);
+console.log("subtraction", x - y);
+console.log("muliplication", x * y);
+console.log("division", x / y);
+console.log("modulus", x % y);
+console.log("exponentiation", x ** y);
